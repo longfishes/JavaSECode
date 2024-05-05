565e542fc080d6ea5e682893b8f132b4008ef93d
longfish <longfishes@qq.com>
2024-05-05 21:45:40 +0800

    1652. 拆炸弹

---
+++ Examination/src/com/longfish/lc2024/month04/month05/exer01.java
@@ -0,0 +1,52 @@
+package com.longfish.lc2024.month04.month05;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class exer01 {
+    public int[] decrypt(int[] code, int k) {
+        int n = code.length;
+        int[] decode = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (k > 0) {
+                int cnt = 0;
+                for (int j = 1; j <= k; j++) {
+                    cnt += code[loop(i + j, n)];
+                }
+                decode[i] = cnt;
+            }
+            else if (k < 0) {
+                int cnt = 0;
+                for (int j = 1; j <= -k; j++) {
+                    cnt += code[loop(i - j, n)];
+                }
+                decode[i] = cnt;
+            } else return decode;
+        }
+        return decode;
+    }
+
+    private int loop(int k, int len) {
+        if (k == 0) return 0;
+        while (k < 0) {
+            k += len;
+        }
+        return k % len;
+    }
+
+    @Test
+    public void test3() {
+        System.out.println(Arrays.toString(decrypt(new int[]{5, 7, 1, 4}, 3)));
+    }
+
+    @Test
+    public void test2() {
+        System.out.println(loop(4, 4));
+    }
+
+    @Test
+    public void test1() {
+        System.out.println(loop(-1, 2));
+    }
+}
